546655c68892f8ef05d40e789402b7415d84dac4
Miguel Nieva <miguelnieva@conexionew.com>
2017-04-20 16:09:12 +0000

    Base

---
+++ assets/js/theme-options.js
@@ -0,0 +1,44 @@
+/**
+ * You can configure theme options in this file.
+ * Please refer to the documentation for more information.
+ */
+
+/************** Comments **************/
+
+var disqus = "";
+
+/************** Lightbox **************/
+
+var lightbox = true;
+
+/*********** Infinite Scroll **********/
+
+var infinite_scroll = "";
+
+/******** Featured Posts Slider ********/
+
+var slider_auto_slide = true; //slide automatically, true or false
+var slider_pagination = true; //display slider pagination, true or false
+var slider_nav = true; //display slider navigation (the arrows), true or false
+var slider_transition_speed = 500; //speed of transition, in milliseconds
+var slider_image_timeout = 4000; //time between slide transitions, in milliseconds
+var slider_pause = true; //pause on hover, true or false
+var slider_keyboard = true; //control the slider using direction arrows (keyboard)
+
+/************ Social Media ************/
+
+var google_plus_link = "";
+var dribbble_link = "";
+var instagram_link = "https://instagram.com/";
+var tumblr_link = "";
+var youtube_link = "";
+var vimeo_link = "";
+var pinterest_link = "";
+var flickr_link = "";
+var linkedin_link = "";
+var github_link = "https://www.github.com";
+var rss_link = false;
+
+/********* Post Title Limit *********/
+
+var post_title_limit = "92"
